bf3396653389ee8914833013adc31517f4faaced
sdvic <gvw@san.rr.com>
2014-08-24 15:49:15 -0700

    version 140824A Working with report date and total Student Hours  * Scholarship Hours????

--- SudentHours/src/TimeReporter.java
+++ SudentHours/src/TimeReporter.java
@@ -1,11 +1,13 @@
 /****************************************************************************************
  * Application to extract Student hours by name from Quick Books Invoice dump
- * Vic Wintriss version 140822A Incorporating Erik's clean-up...trying to extract report date
+ * Vic Wintriss version 140824A Working with report date and total Student Hours
+ * Scholarship Hours????
  ****************************************************************************************/
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -21,10 +23,14 @@ public class TimeReporter {
 
 	private static final int NAME_CELL = 5;// column 5 Name
 	private static final int MEMO_CELL = 4;// column 4 Memo
-	private static final int DATE_CELL = 5;// column 4 Date
+	private static final int DATE_CELL = 2;// column 3 Date
 	private static final Pattern TUITION_PATTERN = Pattern
 			.compile("tuition - (\\d*\\.?\\d+) hr session");
-	private static String reportDate;
+	private static Date reportDate;
+	private static double totalMonthStudentHours;
+	private static Student student = new Student("initial");
+	private static String studentName;
+	private static double totalMonthScholarshipHours;
 
 	public static void main(String[] args) throws IOException {
 		TimeReporter reporter = new TimeReporter();
@@ -40,22 +46,28 @@ public class TimeReporter {
 		for (Row row : sheet) {
 			Cell name = row.getCell(NAME_CELL);
 			Cell memo = row.getCell(MEMO_CELL);
-			Cell date = row.createCell(DATE_CELL);
-		
+			Cell dateSring = row.getCell(DATE_CELL);
+
 			double classHours = 0.0;
 			if (memo != null && name != null) {
 
 				String classHoursString = memo.toString();
+				String scholarshipString = memo.toString();
 				Matcher m = TUITION_PATTERN.matcher(classHoursString);
 				if (m.matches()) {
 					classHours = Double.parseDouble(m.group(1));
 				} else if (classHoursString.contains("workshop")) {
 					classHours = 10.0;
 				}
+				if(scholarshipString.contains("scholarship"))
+				{
+					totalMonthScholarshipHours += 10;
+				}
 
 				if (classHours != 0.0) {
-					String studentName = name.getStringCellValue();
-					Student student = database.get(studentName);
+					studentName = name.getStringCellValue();
+					reportDate = dateSring.getDateCellValue();
+					student = database.get(studentName);
 					if (student == null) {
 						student = new Student(studentName);
 						database.put(studentName, student);
@@ -67,13 +79,17 @@ public class TimeReporter {
 	}
 
 	private void printStudents() {
-		// System.out.println("Month Student Hour Summary as of \n\n" +
-		// reportDate);
+		System.out.println("Monthly Student Hour Summary as of " + reportDate
+				+ "\n");
 		Student[] students = new Student[database.size()];
 		students = database.values().toArray(students);
 		Arrays.sort(students);
 		for (Student s : students) {
 			System.out.println(s);
+			
+			totalMonthStudentHours += s.getHours();
 		}
+		System.out.println("\nTotal Student hours for this month " + totalMonthStudentHours);
+		System.out.println("\nTotal Scholarship hours for this month " + totalMonthScholarshipHours);
 	}
 }
